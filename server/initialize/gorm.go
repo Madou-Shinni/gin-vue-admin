c0f60313a40bbe87e3404688037392f80cca27bd
ricardo <ohcanikissu@gmail.com>
2024-02-21 13:17:57 +0800

    first commit

---
+++ server/initialize/gorm.go
@@ -0,0 +1,60 @@
+package initialize
+
+import (
+	"os"
+
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/example"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/system"
+
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+func Gorm() *gorm.DB {
+	switch global.GVA_CONFIG.System.DbType {
+	case "mysql":
+		return GormMysql()
+	case "pgsql":
+		return GormPgSql()
+	case "oracle":
+		return GormOracle()
+	case "mssql":
+		return GormMssql()
+	case "sqlite":
+		return GormSqlite()
+	default:
+		return GormMysql()
+	}
+}
+
+func RegisterTables() {
+	db := global.GVA_DB
+	err := db.AutoMigrate(
+
+		system.SysApi{},
+		system.SysUser{},
+		system.SysBaseMenu{},
+		system.JwtBlacklist{},
+		system.SysAuthority{},
+		system.SysDictionary{},
+		system.SysOperationRecord{},
+		system.SysAutoCodeHistory{},
+		system.SysDictionaryDetail{},
+		system.SysBaseMenuParameter{},
+		system.SysBaseMenuBtn{},
+		system.SysAuthorityBtn{},
+		system.SysAutoCode{},
+		system.SysExportTemplate{},
+
+		example.ExaFile{},
+		example.ExaCustomer{},
+		example.ExaFileChunk{},
+		example.ExaFileUploadAndDownload{},
+	)
+	if err != nil {
+		global.GVA_LOG.Error("register table failed", zap.Error(err))
+		os.Exit(0)
+	}
+	global.GVA_LOG.Info("register table success")
+}
